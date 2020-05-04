3b9ab51dbbd9b14f922eb74978414c782c070290
Frank Mueller <mail@themue.dev>
2020-05-04 22:41:17 +0200

    Continue testing the stay-set indicator
    Signed-off-by: Frank Mueller <mail@themue.dev>

--- stayset/stayset_test.go
+++ stayset/stayset_test.go
@@ -8,11 +8,41 @@
 package stayset_test // import "tideland.dev/go/trace/stayset"
 
 import (
+	"sync"
 	"testing"
+	"time"
 
 	"tideland.dev/go/audit/asserts"
+	"tideland.dev/go/audit/generators"
 	"tideland.dev/go/trace/stayset"
 )
+
+// generateIndicators creates some indicators for the tests.
+func generateIndicators() {
+	gen := generators.New(generators.FixedRand())
+	ssiOne := stayset.ForNamespace("one")
+	ssiOneA := ssiOne.IndicatorPoint("a")
+	ssiOneB := ssiOne.IndicatorPointWithValue("b", 10)
+	ssiTwo := stayset.ForNamespace("two")
+	ssiTwoA := ssiTwo.IndicatorPoint("a")
+	points := []*stayset.IndicatorPoint{ssiOneA, ssiOneB, ssiTwoA}
+
+	var wg sync.WaitGroup
+	wg.Add(2500)
+
+	for j := 0; j < 2500; j++ {
+		go func() {
+			b := gen.OneByteOf(0, 1, 2, 1, 2, 2)
+			point := points[b]
+			i := point.Start()
+			gen.SleepOneOf(1*time.Millisecond, 2*time.Millisecond, 4*time.Millisecond)
+			i.Stop()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+}
 
 //--------------------
 // TESTS
@@ -38,6 +68,22 @@ func TestCreateSSI(t *testing.T) {
 	assert.NotNil(ssiReuse)
 	assert.Different(ssiReuse, ssiTwo)
 	assert.Equal(ssiOne, ssiReuse)
+}
+
+// TestIndicators runs a number of stay-set indications.
+func TestIndicators(t *testing.T) {
+	assert := asserts.NewTesting(t, asserts.FailStop)
+
+	generateIndicators()
+
+	// Only for one indicator.
+	iv := stayset.ForNamespace("one").IndicatorPoint("a").Value()
+	assert.Equal(iv.Namespace, "one")
+	assert.Equal(iv.ID, "a")
+	assert.Range(iv.Quantity, 400, 450)
+	assert.Equal(iv.Minimum, 0)
+	assert.Range(iv.Maximum, 250, 300)
+	assert.Logf("%v", iv)
 }
 
 // EOF
