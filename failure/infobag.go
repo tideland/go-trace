daeb3693eba7c6345ecf39eb8935e5743146d06f
themue <frank.mueller@themue.dev>
2020-08-01 18:47:49 +0000

    Base implementation of InfoBag
    Signed-off-by: themue <frank.mueller@themue.dev>

--- failure/infobag.go
+++ failure/infobag.go
@@ -11,6 +11,11 @@ package failure // import "tideland.dev/go/trace/failure"
 // IMPORTS
 //--------------------
 
+import (
+	"fmt"
+	"strings"
+)
+
 //--------------------
 // INFO
 //--------------------
@@ -22,8 +27,74 @@ type info struct {
 	value interface{}
 }
 
+// String implements the fmt.Stringer interface.
+func (i info) String() string {
+	switch i.value.(type) {
+	case string:
+		return fmt.Sprintf("%q: %q", i.key, i.value)
+	default:
+		return fmt.Sprintf("%q: %v", i.key, i.value)
+	}
+}
+
 //--------------------
 // INFO BAG
 //--------------------
+
+// InfoBag contains a number of useful extra informations for
+// failures.
+type InfoBag struct {
+	infos []info
+}
+
+// NewInfoBag creates an InfoBag with a number of keys and values.
+// The arguments are interpreted as a alternating pairs of those.
+// So keys are taken as or converted into strings. In case the final
+// item would be a key its value will be set to true.
+func NewInfoBag(kvs ...interface{}) *InfoBag {
+	ib := &InfoBag{}
+	i := info{}
+	for _, kv := range kvs {
+		// Check for new key.
+		if i.key == "" {
+			i.key = fmt.Sprintf("%s", kv)
+			continue
+		}
+		// Now a value. Add the info to the bag.
+		i.value = kv
+		ib.infos = append(ib.infos, i)
+		i = info{}
+	}
+	// Check if loop ended after key.
+	if i.key != "" {
+		i.value = true
+		ib.infos = append(ib.infos, i)
+	}
+	return ib
+}
+
+// Len returns the number of informations inside of
+// the InfoBag.
+func (ib InfoBag) Len() int {
+	return len(ib.infos)
+}
+
+// Do iterates over the informations of the InfoBag and
+// calls the given function for each key and value.
+func (ib InfoBag) Do(f func(key string, valiue interface{})) {
+	for _, i := range ib.infos {
+		f(i.key, i.value)
+	}
+}
+
+// String implements the fmt.Stringer interface.
+func (ib InfoBag) String() string {
+	kvs := make([]string, len(ib.infos))
+	for i, info := range ib.infos {
+		kvs[i] = info.String()
+	}
+	kvss := strings.Join(kvs, ",")
+	return "{" + kvss + "}"
+}
 
 // EOF
---
+++ failure/infobag_test.go
@@ -0,0 +1,51 @@
+// Tideland Go Trace - Failure - Unit Tests
+//
+// Copyright (C) 2020 Frank Mueller / Tideland / Oldenburg / Germany
+//
+// All rights reserved. Use of this source code is governed
+// by the new BSD license.
+
+package failure_test
+
+//--------------------
+// IMPORTS
+//--------------------
+
+import (
+	"testing"
+
+	"tideland.dev/go/audit/asserts"
+	"tideland.dev/go/trace/failure"
+)
+
+//--------------------
+// TESTS
+//--------------------
+
+// TestNewInfoBag verifies the correct instantiation of a
+// an InfoBag.
+func TestNewInfoBag(t *testing.T) {
+	assert := asserts.NewTesting(t, asserts.FailStop)
+	values := map[string]interface{}{
+		"a": "foo",
+		"b": 12345,
+		"c": 13.37,
+		"d": false,
+		"e": true,
+	}
+	ib := failure.NewInfoBag(
+		"a", values["a"],
+		"b", values["b"],
+		"c", values["c"],
+		"d", values["d"],
+		"e",
+	)
+
+	assert.Length(ib, 5)
+
+	ib.Do(func(key string, value interface{}) {
+		assert.Equal(value, values[key])
+	})
+}
+
+// EOF
